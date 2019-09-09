d36925a052252043e3dfd237757d7d59316c7e5c
jacob-bonner <54957118+jacob-bonner@users.noreply.github.com>
2019-09-09 18:56:22 -0400

    Add files via upload

---
+++ cpp_name-address.cpp
@@ -0,0 +1,10 @@
+//This program outputs your name and where you live
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Jacob" << std::endl;
+	std::cout << "Ontario" << std::endl;
+	std::cout << "Canada" << std::endl;
+}
